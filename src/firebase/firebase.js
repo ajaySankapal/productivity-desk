cf77b47840a9c83115f2d0be12a0ede66411879c
ajaySankapal <90617524+ajaySankapal@users.noreply.github.com>
2022-02-19 19:14:10 +0530

    connected firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,13 +10,19 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWBTLJadHM7ShUDL86OCk6WaBfNbIdZlo",
-  authDomain: "select-d389a.firebaseapp.com",
-  projectId: "select-d389a",
-  storageBucket: "select-d389a.appspot.com",
-  messagingSenderId: "965509538880",
-  appId: "1:965509538880:web:1eec7ddfd8afd3d491aba5",
-  measurementId: "G-SCBVBLDPRF",
+  // apiKey: "AIzaSyBWBTLJadHM7ShUDL86OCk6WaBfNbIdZlo",
+  // authDomain: "select-d389a.firebaseapp.com",
+  // projectId: "select-d389a",
+  // storageBucket: "select-d389a.appspot.com",
+  // messagingSenderId: "965509538880",
+  // appId: "1:965509538880:web:1eec7ddfd8afd3d491aba5",
+  // measurementId: "G-SCBVBLDPRF",
+  apiKey: "AIzaSyBti7lia_haWj4r5dMxPtvrt6TG_T9qiss",
+  authDomain: "productivity-desk.firebaseapp.com",
+  projectId: "productivity-desk",
+  storageBucket: "productivity-desk.appspot.com",
+  messagingSenderId: "640410957431",
+  appId: "1:640410957431:web:69e9f6217649f8580f3872",
 };
 
 // Initialize Firebase
